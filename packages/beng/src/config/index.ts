a0569ee2d8ac613741625f4e54e75a48e4226efa
AkaraChen <akarachen@outlook.com>
2023-10-14 20:00:07 +0800

    fix: build on windows

--- packages/beng/src/config/index.ts
+++ packages/beng/src/config/index.ts
@@ -7,11 +7,18 @@ import consola from 'consola';
 const wd = process.cwd();
 export const configFilePath = slash(path.resolve(wd, 'beng.config.mjs'));
 
+export function formatImportPath(path: string) {
+	if (process.platform === 'win32') {
+		return `file://${slash(path)}`;
+	}
+	return path;
+}
+
 export async function resolveBengConfig(): Promise<BengConfig> {
 	if (!fs.existsSync(configFilePath)) {
 		return {};
 	}
 	consola.info(`Using config file: ${configFilePath}`);
-	const module = await import(configFilePath);
+	const module = await import(formatImportPath(configFilePath));
 	return module.default;
 }
