de0d44c27cb6df3f20eaee3ca6a78851e16cc591
无尘室内点墨 <akarachen@outlook.com>
2024-08-05 20:10:25 +0800

    feat: bengbuzhule

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,10 +1,29 @@
-import execa from "execa";
+import { execa } from "execa";
+import { ensureDependencyInstalled, detectPackageManager } from "nypm";
 
-await execa("npx", [
-	"--package",
-	"@bengbu/core",
-	"--package",
-	"rollup",
-	"--",
-	"rollup -c node:@beng.core",
-]);
+await ensureDependencyInstalled("@bengbu/core");
+await ensureDependencyInstalled("rollup");
+await ensureDependencyInstalled("@bengbu/zhule");
+
+let manager = await detectPackageManager(process.cwd()).then((m) => m.name);
+if (manager === undefined) {
+	manager = "npm";
+}
+
+switch (manager) {
+	case "npm": {
+		await execa("npx", ["rollup", "-c", "node:@bengbu/core"]);
+		break;
+	}
+	case "pnpm": {
+		await execa("pnpm", ["rollup", "-c", "node:@bengbu/core"]);
+		break;
+	}
+	case "yarn": {
+		await execa("yarn", ["rollup", "-c", "node:@bengbu/core"]);
+		break;
+	}
+	default: {
+		throw new Error(`Unsupported package manager: ${manager}`);
+	}
+}
